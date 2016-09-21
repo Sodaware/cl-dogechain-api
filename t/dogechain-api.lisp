40dcc70471e5d81651a2cd3542f256033648f236
Phil Newton <phil@sodaware.net>
2016-09-21 08:58:24 -0400

    Group all tests into `subtest` groups

--- t/dogechain-api.lisp
+++ t/dogechain-api.lisp
@@ -6,7 +6,7 @@
         :cl-mock))
 (in-package :dogechain-api-test)
 
-(plan 41)
+(plan 10)
 
 
 ;; ----------------------------------------------------------------------
@@ -61,116 +61,122 @@
 ;; -- decode-address
 ;; ----------------------------------------------------------------------
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/decode_address/invalid_address")
-      "Error: address invalid"))
-  (is-error (dogechain-api:decode-address "invalid_address") 'dogechain-api-error))
+(subtest ":decode-address"
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/decode_address/valid_address")
-      "1E:hash_goes_here"))
-  (let ((response (dogechain-api:decode-address "valid_address")))
-    (is (cdr (assoc :version response)) "1E")
-    (is (cdr (assoc :hash response)) "hash_goes_here")))
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/decode_address/invalid_address")
+              "Error: address invalid"))
+    (is-error (dogechain-api:decode-address "invalid_address") 'dogechain-api-error))
+
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/decode_address/valid_address")
+              "1E:hash_goes_here"))
+    (let ((response (dogechain-api:decode-address "valid_address")))
+      (is (cdr (assoc :version response)) "1E")
+      (is (cdr (assoc :hash response)) "hash_goes_here"))))
 
 
 ;; ----------------------------------------------------------------------
 ;; -- get-received-by-address
 ;; ----------------------------------------------------------------------
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/getreceivedbyaddress/invalid_address")
-      "Error: address invalid"))
-  (is-error (dogechain-api:get-received-by-address "invalid_address") 'dogechain-api-error))
+(subtest ":get-received-by-address"
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/getreceivedbyaddress/valid_address")
-      "1.2345"))
-  (let ((received-amount (dogechain-api:get-received-by-address "valid_address")))
-    (is received-amount 1.2345)
-    (is-type received-amount 'float)))
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/getreceivedbyaddress/invalid_address")
+              "Error: address invalid"))
+    (is-error (dogechain-api:get-received-by-address "invalid_address") 'dogechain-api-error))
+
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/getreceivedbyaddress/valid_address")
+              "1.2345"))
+    (let ((received-amount (dogechain-api:get-received-by-address "valid_address")))
+      (is received-amount 1.2345)
+      (is-type received-amount 'float))))
 
 
 ;; ----------------------------------------------------------------------
 ;; -- get-sent-by-address
 ;; ----------------------------------------------------------------------
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/getsentbyaddress/invalid_address")
-      "Error: address invalid"))
-  (is-error (dogechain-api:get-sent-by-address "invalid_address") 'dogechain-api-error))
+(subtest ":get-sent-by-address"
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/getsentbyaddress/valid_address")
-      "1.2345"))
-  (let ((sent-amount (dogechain-api:get-sent-by-address "valid_address")))
-    (is sent-amount 1.2345)
-    (is-type sent-amount 'float)))
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/getsentbyaddress/invalid_address")
+              "Error: address invalid"))
+    (is-error (dogechain-api:get-sent-by-address "invalid_address") 'dogechain-api-error))
 
-
-;; ----------------------------------------------------------------------
-;; -- hash-to-address
-;; ----------------------------------------------------------------------
-
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/totalbc/")
-      "12345.6789"))
-  (let ((mined-amount (dogechain-api:get-total-mined)))
-    (is mined-amount 12345.6789)
-    (is-type mined-amount 'float)))
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/getsentbyaddress/valid_address")
+              "1.2345"))
+    (let ((sent-amount (dogechain-api:get-sent-by-address "valid_address")))
+      (is sent-amount 1.2345)
+      (is-type sent-amount 'float))))
 
 
 ;; ----------------------------------------------------------------------
 ;; -- hash-to-address
 ;; ----------------------------------------------------------------------
 
-(is-error (dogechain-api:hash-to-address "hash") 'dogechain-api-error)
+(subtest ":hash-to-address"
+
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/totalbc/")
+              "12345.6789"))
+    (let ((mined-amount (dogechain-api:get-total-mined)))
+      (is mined-amount 12345.6789)
+      (is-type mined-amount 'float)))
+
+  (is-error (dogechain-api:hash-to-address "hash") 'dogechain-api-error))
 
 
 ;; ----------------------------------------------------------------------
 ;; -- Internal Helper Tests
 ;; ----------------------------------------------------------------------
 
-(is (dogechain-api::build-simple-endpoint "testmethod")
-    "http://dogechain.info/chain/Dogecoin/q/testmethod/")
+(subtest "::build-simple-endpoint"
 
-(is (dogechain-api::build-simple-endpoint "testmethod" '("param-1"))
-    "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1")
+  (is (dogechain-api::build-simple-endpoint "testmethod")
+      "http://dogechain.info/chain/Dogecoin/q/testmethod/")
 
-(is (dogechain-api::build-simple-endpoint "testmethod" '("param-1" "param-2"))
-    "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1/param-2")
+  (is (dogechain-api::build-simple-endpoint "testmethod" '("param-1"))
+      "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1")
 
-(is (dogechain-api::build-simple-endpoint "testmethod" '(nil nil nil))
-    "http://dogechain.info/chain/Dogecoin/q/testmethod/")
+  (is (dogechain-api::build-simple-endpoint "testmethod" '("param-1" "param-2"))
+      "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1/param-2")
 
-(with-mocks ()
-  (answer (drakma:http-request uri)
-    (progn
-      (is uri "http://dogechain.info/chain/Dogecoin/q/testmethod/")
-      "test<!-- comments for security -->"))
-  (is (dogechain-api::get-simple "testmethod") "test"))
+  (is (dogechain-api::build-simple-endpoint "testmethod" '(nil nil nil))
+      "http://dogechain.info/chain/Dogecoin/q/testmethod/"))
+
+(subtest "::get-simple"
+
+  (with-mocks ()
+    (answer (drakma:http-request uri)
+            (progn
+              (is uri "http://dogechain.info/chain/Dogecoin/q/testmethod/")
+              "test<!-- comments for security -->"))
+    (is (dogechain-api::get-simple "testmethod") "test")))
 
 
-;; Check HTML comments get stripped
+(subtest "::strip-html-comments"
+  (is (dogechain-api::strip-html-comments "12345<!-- comment -->")
+      "12345")
 
-(is (dogechain-api::strip-html-comments "12345<!-- comment -->")
-    "12345")
-
-(is (dogechain-api::strip-html-comments "12345")
-    "12345")
+  (is (dogechain-api::strip-html-comments "12345")
+      "12345"))
 
 (finalize)
